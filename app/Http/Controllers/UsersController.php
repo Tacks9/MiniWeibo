d5477c7794dede8d6643cf846e3b595304564bbf
Tacks9 <tacks321@qq.com>
2019-11-28 15:55:37 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,11 +22,24 @@ class UsersController extends Controller
     // 注册提交
     public function store(Request $request)
     {
+        // 验证字段
         $this->validate($request ,[
             'name'      => 'required|max:50',
             'email'     => 'required|email|unique:users|max:255',
             'password'  => 'required|confirmed|min:6'
         ]);
-        return;
+
+        // 写入数据库
+        $user = User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password)
+        ]);
+
+        // 保存会话
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        // 路由跳转 绑定数据
+        return redirect()->route('users.show',[$user]);
     }
 }
