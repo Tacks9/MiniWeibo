b6cf1e8496e900b41116fb9eaeda33a2d115d3f6
Tacks9 <tacks321@qq.com>
2019-11-28 15:16:38 +0800

    用户页面显示

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 
 class User extends Authenticatable
 {
@@ -36,4 +37,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // 获取头像 gravatar
+    public function gg($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,31 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 // 注册页面
 Route::get('/signup', 'UsersController@create')->name('signup');
+
+
+/************** 用户资源页面 *********************/
+Route::resource('users', 'UsersController');
+// 等同下面的
+//---------------------------------读取-----------------------------------
+// 显示所有用户列表的页面
+// Route::get('/users',       'UsersController@index')->name('users.index');
+// // 显示某个用户
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+
+// //--------------------------------新增-------------------------------------
+// // 新增页面
+// Route::get('/users/create', 'UsersController@create')->name('users.create');
+// // 新增提交
+// Route::post('/users',        'UsersController@store')->name('users.store');
+
+// // ------------------------------更新--------------------------------------
+// // 修改页面
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// // 修改提交
+// Route::patch('/users/{user}',  'UsersController@update')->name('users.update');
+
+// // ------------------------------删除------------------------------------------------
+// // 删除
+// Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+
+
