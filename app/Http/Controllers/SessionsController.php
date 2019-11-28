535711e3d556aeb56e8c8a7932f20cc7f8903b9f
Tacks9 <tacks321@qq.com>
2019-11-28 17:53:52 +0800

    登录会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+
+class SessionsController extends Controller
+{
+    // 登录页面
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    // 登录认证
+    public function store(Request $request)
+    {
+        // 参数验证
+        $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
+        // 用户名密码比对
+         if (Auth::attempt($credentials)) {
+           // 登录成功后的相关操作
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+           // 登录失败后的相关操作
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+
+
+        return ;
+    }
+}
